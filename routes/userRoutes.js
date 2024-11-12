278b463d40bb2c27df0895f0dc8d5d70dec47bdf
KirillBazilsky <bazilsky.fortech@gmail>
2024-11-12 14:10:09 +0300

    init project

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/usersControllers');
-
-router.post('/', userController.createUser);  
-router.put('/:userId', userController.updateUser);
-
-module.exports = router;
